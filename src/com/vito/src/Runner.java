d001287f662df8163c21837da9a59405d0bb3ff6
Vitaliy Ippolitov <tobestmail@gmail.com>
2022-01-23 13:41:50 +0300

    Add logic into processPrimeFactors and processNumbers

--- src/com/vito/src/Runner.java
+++ src/com/vito/src/Runner.java
@@ -3,26 +3,37 @@ package com.vito.src;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
 
-import static java.lang.System.out;
 import static java.util.Arrays.stream;
 
 public class Runner {
 
     private static final AtomicInteger ai = new AtomicInteger(0); // TODO rm
 
-    public static void processPrimeFactors(int number) {
+    public static String processPrimeFactors(int number) {
+        if (4 == number) {
+            return "";
+        }
         var primeNumbers = PollardRho.primeFactors(number);
         if (primeNumbers.size() == 1) {
-            out.println(number + " 1");
+            return number + " 1\n";
         } else {
-            out.println("idx: " + ai.getAndIncrement() + " number:" + number + ", result: " + primeNumbers);
+            var iterationCount = 1;
+            var factorPrimeNumbers = primeNumbers;
+            do {
+                var sum = factorPrimeNumbers.stream().reduce(0, Integer::sum);
+                factorPrimeNumbers = PollardRho.primeFactors(sum);
+                iterationCount++;
+            } while (factorPrimeNumbers.size() > 1);
+
+            return factorPrimeNumbers.get(0) + " " + iterationCount + "\n";
         }
     }
 
     public static String processNumbers(IntStream numbers) {
-        numbers.forEach(Runner::processPrimeFactors);
+        StringBuilder result = new StringBuilder();
+        numbers.forEach(num -> result.append(processPrimeFactors(num)));
 
-        return null;
+        return result.toString();
     }
 
     public static void main(String[] args) {
