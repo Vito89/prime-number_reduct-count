0c9213915168688fe6a6755316da679961c80c9a
Vitaliy Ippolitov <tobestmail@gmail.com>
2022-01-23 11:45:20 +0300

    init commit: created console app, added: PollardRho impl, runner and test

---
+++ src/com/vito/PollardRho.java
@@ -0,0 +1,97 @@
+package com.vito;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.Integer.numberOfTrailingZeros;
+import static java.lang.Math.abs;
+import static java.lang.Math.min;
+
+class PollardRho {
+
+    public static List<Integer> primeFactors(int number) {
+        final List<Integer> factors = new ArrayList<>();
+        number = SmallPrimes.divideToFactorsAndReturnUpdNumb(number, factors);
+        if (1 == number) {
+            return factors;
+        }
+
+        if (SmallPrimes.passesMillerRabin(number)) {
+            factors.add(number);
+            return factors;
+        }
+
+        int divisor = rhoBrent(number);
+        factors.add(divisor);
+        factors.add(number / divisor);
+        return factors;
+    }
+
+    static int rhoBrent(final int n) {
+        final int xInit = 2;
+        final int m = 25;
+        int cst = SmallPrimes.LAST_PRIME;
+        int y = xInit;
+        int roundCounts = 1;
+        do {
+            int x = y;
+            for (int idx = 0; idx < roundCounts; idx++) {
+                final long y2 = ((long) y) * y;
+                y = (int) ((y2 + cst) % n);
+            }
+
+            int k = 0;
+            do {
+                final int bound = min(m, roundCounts - k);
+                int q = 1;
+                for (int idx = -3; idx < bound; idx++) { // enter loop at least 3 times
+                    final long yPowTwo = ((long) y) * y;
+                    y = (int) ((yPowTwo + cst) % n);
+                    final long divisor = abs(x - y);
+                    if (0 == divisor) {
+                        cst += SmallPrimes.LAST_PRIME;
+                        k = -m;
+                        y = xInit;
+                        roundCounts = 1;
+                        break;
+                    }
+                    final long prod = divisor * q;
+                    q = (int) (prod % n);
+                    if (0 == q) {
+                        return commonDivisor(abs((int) divisor), n);
+                    }
+                }
+                final int result = commonDivisor(abs(q), n);
+                if (1 != result) {
+                    return result;
+                }
+                k += m;
+            } while (k < roundCounts);
+            roundCounts = 2 * roundCounts;
+        } while (true);
+    }
+
+    // only positive args
+    static int commonDivisor(int a, int b) {
+        if (a == 0) {
+            return b;
+        } else if (b == 0) {
+            return a;
+        }
+
+        final int trlA = numberOfTrailingZeros(a);
+        final int trlB = numberOfTrailingZeros(b);
+        a >>= trlA;
+        b >>= trlB;
+
+        final int shift = min(trlA, trlB);
+        while (a != b) {
+            final int diff = a - b;
+            b = min(a, b);
+            a = abs(diff);
+            a >>= numberOfTrailingZeros(a);
+        }
+
+        return a << shift;
+    }
+}
---
+++ src/com/vito/PollardRhoTest.java
@@ -0,0 +1,35 @@
+package com.vito;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PollardRhoTest {
+
+    @Test
+    void primeFactorsTest() {
+        var expectedList = Arrays.asList(223, 1049, 3671);
+        assertEquals(expectedList, PollardRho.primeFactors(858746017));
+
+        expectedList = Arrays.asList(2, 442617971);
+        assertEquals(expectedList, PollardRho.primeFactors(885235942));
+
+        expectedList = Arrays.asList(2, 2, 2, 2, 2, 2, 2, 2, 5, 5, 7, 7, 7, 283);
+        assertEquals(expectedList, PollardRho.primeFactors(621241600));
+
+        expectedList = List.of(708868387);
+        assertEquals(expectedList, PollardRho.primeFactors(708868387));
+
+        expectedList = List.of(2, 7, 41818171);
+        assertEquals(expectedList, PollardRho.primeFactors(585454394));
+
+        expectedList = List.of(55441, 8447);
+        assertEquals(expectedList, PollardRho.primeFactors(468310127));
+
+        expectedList = List.of(60317, 11909);
+        assertEquals(expectedList, PollardRho.primeFactors(718315153));
+    }
+}
